a8a599d5a40a172a06aa543f66b3942ff08af5b7
ausi1997.com <ausafen1018@gmail.com>
2021-03-16 18:40:55 +0530

    socket connection enabled

--- hike-chat/src/components/chat/index.js
+++ hike-chat/src/components/chat/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
+let socket;
+const ChatRoom = ({location})=>{
+    
+    const [name, setName] = useState('');
+    const [room , setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
-const ChatRoom = ()=>{
+    useEffect(()=>{
+    const {name , room}= queryString.parse(location.search);
+
+    socket = io("localhost:6000");
+    setName(name);
+    setRoom(room); 
+  //  console.log(socket);
+socket.emit("join", {name,room},({error})=>{
+    alert(error);
+});
+return ()=>{
+    socket.emit('disconnect');
+
+    socket.off();
+}
+    },["localhost:6000", location.search]);
+
+    useEffect(()=>{
+        socket.on('message', ()=>{
+         setMessages([...messages,message]);
+        })
+    },[messages]);
+
+    // function to send messages
+
+    
     return(
         <h1>Chat</h1>
     )
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const {addUser,removeUser,getUser,getUserInRoom} = require('./user');
 
 const router = require('./router');
 
@@ -18,6 +19,28 @@ const io = socketio(server);
 // setting up a new socket connection
 io.on("connection" , (socket)=>{
  console.log("New Connection...");
+
+ socket.on("join" , ({name,room}, callback)=>{
+     const {error , user } = addUser({id:socket.id , name ,room});
+
+     if(error){
+         return callback(error);
+     }
+       socket.emit('message', {user:'admin', text:`${user.name} Welcome to the room ${user.room}`});
+       socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name} has joined`});   
+
+         socket.join(user.room);
+
+         callback();
+ });
+socket.on('sendMessage', (message,callback)=>{
+    const user = getUser(socket.io);
+
+    io.to(user.room).emit('message', {user:user.name , text:message});
+
+    callback();
+    
+})
 
  socket.on("disconnect" ,()=>{
      console.log("Disconnected");
